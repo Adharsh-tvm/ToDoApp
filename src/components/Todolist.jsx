83a5a7f0d9020874665dc57c772a6a8406d75db9
Adharsh S <adharshkattaikonam@gmail.com>
2025-01-14 09:28:23 +0530

    Sorting addeddd

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -2,9 +2,12 @@ import TodoItem from "./TodoItem";
 import styles from './todolist.module.css';
 
 export default function Todolist({ todos, setTodos }) {
+  // Sort todos: incomplete items first, then completed ones
+  const sortedTodos = [...todos].sort((a, b) => a.done - b.done);
+
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem key={item.id} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
